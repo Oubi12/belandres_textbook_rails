12fa9a94d58edcfe3ccb2c73c7644f08a6d680c8
Oubi12 <robiehbels@gmail.com>
2023-09-27 17:42:57 +0800

    about CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
   get :home, to:'welcome#home'
   get :about, to: 'welcome#about'
   get :contact, to: 'welcome#contact'
+  resources :posts
 end
